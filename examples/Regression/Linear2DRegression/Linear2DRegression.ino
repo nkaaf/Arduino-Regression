c761b3638b13cd38c4b269de635c70cc4bf2936a
nkaaf <nkaaf@protonmail.com>
2023-01-05 22:55:48 +0100

    init

---
+++ examples/Regression/Linear2DRegression/Linear2DRegression.ino
@@ -0,0 +1,29 @@
+#include <Linear2DRegression.hpp>
+
+// Create a new Regression that will be learned in setup
+Linear2DRegression *linear2DRegression = new Linear2DRegression();
+
+void setup() {
+    Serial.begin(9600);
+
+    // Set points of a linear graph: y = 2 * x + 10
+    linear2DRegression->setPoint(0, 10);
+    linear2DRegression->setPoint(5, 20);
+
+    // Test functionality with a simple x-value => 10
+    Serial.println("Simple test: y = 2 * 10 + 10 => y should be 30");
+    Serial.print("y = ");
+    Serial.println(linear2DRegression->calculate(10));
+
+    // print all corresponding values of the function from 0 to 100
+    for (int i = 0; i < 100; ++i) {
+        Serial.print(i);
+        Serial.print(": y = ");
+        Serial.println(linear2DRegression->calculate(i));
+    }
+
+    // Clear regression
+    linear2DRegression->reset();
+}
+
+void loop() {}
