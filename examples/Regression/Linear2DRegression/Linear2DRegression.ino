fd18e18b88ac755da35cbf2e39dcf6506cc595d3
nkaaf <nkaaf@protonmail.com>
2023-01-05 23:07:08 +0100

    format with clangd

--- examples/Regression/Linear2DRegression/Linear2DRegression.ino
+++ examples/Regression/Linear2DRegression/Linear2DRegression.ino
@@ -4,26 +4,26 @@
 Linear2DRegression *linear2DRegression = new Linear2DRegression();
 
 void setup() {
-    Serial.begin(9600);
+  Serial.begin(9600);
 
-    // Set points of a linear graph: y = 2 * x + 10
-    linear2DRegression->setPoint(0, 10);
-    linear2DRegression->setPoint(5, 20);
+  // Set points of a linear graph: y = 2 * x + 10
+  linear2DRegression->setPoint(0, 10);
+  linear2DRegression->setPoint(5, 20);
 
-    // Test functionality with a simple x-value => 10
-    Serial.println("Simple test: y = 2 * 10 + 10 => y should be 30");
-    Serial.print("y = ");
-    Serial.println(linear2DRegression->calculate(10));
+  // Test functionality with a simple x-value => 10
+  Serial.println("Simple test: y = 2 * 10 + 10 => y should be 30");
+  Serial.print("y = ");
+  Serial.println(linear2DRegression->calculate(10));
 
-    // print all corresponding values of the function from 0 to 100
-    for (int i = 0; i < 100; ++i) {
-        Serial.print(i);
-        Serial.print(": y = ");
-        Serial.println(linear2DRegression->calculate(i));
-    }
+  // print all corresponding values of the function from 0 to 100
+  for (int i = 0; i < 100; ++i) {
+    Serial.print(i);
+    Serial.print(": y = ");
+    Serial.println(linear2DRegression->calculate(i));
+  }
 
-    // Clear regression
-    linear2DRegression->reset();
+  // Clear regression
+  linear2DRegression->reset();
 }
 
 void loop() {}
--- src/Linear2DRegression.cpp
+++ src/Linear2DRegression.cpp
@@ -1,25 +1,26 @@
 /*!
-* @file Linear2DRegression.cpp
-*
-* This file is part of the Regression library. It provides classes representing different types of regressions.
-*
-* Copyright (C) 2023  Niklas Kaaf
-*
-* This library is free software; you can redistribute it and/or
-* modify it under the terms of the GNU Lesser General Public
-* License as published by the Free Software Foundation; either
-* version 2.1 of the License, or (at your option) any later version.
-*
-* This library is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-* Lesser General Public License for more details.
-*
-* You should have received a copy of the GNU Lesser General Public
-* License along with this library; if not, write to the Free Software
-* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
-* USA
-*/
+ * @file Linear2DRegression.cpp
+ *
+ * This file is part of the Regression library. It provides classes representing
+ * different types of regressions.
+ *
+ * Copyright (C) 2023  Niklas Kaaf
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+ * USA
+ */
 
 #include "Linear2DRegression.hpp"
 
@@ -32,31 +33,32 @@
 // b => slope
 // e => residual (error)
 void Linear2DRegression::setPoint(double x, double y) {
-    this->n++;
+  this->n++;
 
-    this->sumX += x;
-    this->sumX2 += pow(x, 2);
-    this->sumY += y;
-    this->sumY2 += pow(y, 2);
-    this->sumXY += x * y;
+  this->sumX += x;
+  this->sumX2 += pow(x, 2);
+  this->sumY += y;
+  this->sumY2 += pow(y, 2);
+  this->sumXY += x * y;
 
-    this->intercept = ((sumY * sumX2) - (sumX * sumXY)) / (n * sumX2 - pow(sumX, 2));
-    this->slope = ((n * sumXY) - (sumX * sumY)) / (n * sumX2 - pow(sumX, 2));
+  this->intercept =
+      ((sumY * sumX2) - (sumX * sumXY)) / (n * sumX2 - pow(sumX, 2));
+  this->slope = ((n * sumXY) - (sumX * sumY)) / (n * sumX2 - pow(sumX, 2));
 }
 
 double Linear2DRegression::calculate(double x) {
-    return (this->slope * x) + this->intercept;
+  return (this->slope * x) + this->intercept;
 }
 
 void Linear2DRegression::reset() {
-    this->n = 0;
+  this->n = 0;
 
-    this->sumX = 0;
-    this->sumX2 = 0;
-    this->sumY = 0;
-    this->sumY2 = 0;
-    this->sumXY = 0;
+  this->sumX = 0;
+  this->sumX2 = 0;
+  this->sumY = 0;
+  this->sumY2 = 0;
+  this->sumXY = 0;
 
-    this->intercept = 0;
-    this->slope = 0;
+  this->intercept = 0;
+  this->slope = 0;
 }
